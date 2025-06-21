bea4e102a26520f09c3713416f2955994650dba9
Ren Yamazaki <r26763041@gmail.com>
2025-06-22 05:25:51 +0900

    HTML/CSSの作成

--- index.php
+++ index.php
@@ -1,5 +1,46 @@
-<?php
-echo "Hello PHP";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>2ちゃんねる掲示板</title>
+  <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+<body>
+  <header>
+    <h1 class="title">2チャンネル掲示板</h1>
+    <hr>
+  </header>
 
-
-//localhost:8080/2chan-bbs-udemy
+  <div class="threadWrapper">
+    <div class="childWrapper">
+      <div class="threadTitle">
+        <span>【タイトル】</span>
+          <h1>2チャンネル掲示板を作ってみた</h1>
+      </div>
+      <section>
+        <article>
+          <div class="wrapper">
+            <div class="nameArea">
+              <span>名前:</span>
+              <p class="username">れんですよん</p>
+              <time>:2025/6/22</time>
+            </div>
+            <p class="comment">手書きのコメントです。</p>
+          </div>
+        </article>
+      </section>
+      <form class="formWrapper">
+        <div>
+          <input type="submit" value="書き込む">
+          <label>名前:</label>
+          <input type="text">
+        </div>
+        <div>
+          <textarea class="commentTextArea"></textarea>
+        </div>
+      </form>
+    </div>
+  </div>
+</body>
+</html>
